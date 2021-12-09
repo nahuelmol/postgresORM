c72963ad48129ca0ed7aea1c12425d0e746cf168
Silvio Molina <molinahuel44@gmail.com>
2021-12-09 13:06:30 -0300

    verifying update statement

--- APImeths/apis.js
+++ APImeths/apis.js
@@ -27,6 +27,39 @@ const Create = (table, data) => {
 	}
 }
 
+const Update = (table,data) => {
+	var req_data = `SELECT * FROM ${table};`
+
+	var data_arr = Object.values(data)
+	var new_data = ''
+	var old_cols = ''
+
+	var i = 0
+
+	var catcher = (err,res) => {
+		if(err){
+			console.log('data does not found')
+			return
+		}
+
+		res.forEach(each => {
+			old_cols.push(each)
+		})
+
+		data_arr.forEach(each => {
+			new_data = new_data + old_cols[i] + each
+			i++
+		})
+
+		var sente = `UPDATE ${table}
+					SET column1 = ${data_arr[0]}, column2 = value2, ...
+					WHERE condition; `
+	}
+
+	client.query(req_data, catcher)
+
+}
+
 module.exports = {
 	Delete,
 	Create,
